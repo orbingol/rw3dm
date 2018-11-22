a075696fd00d0454c24ac4d5bdaf8c38cda58e05
Onur R. Bingol <orbingol@iastate.edu>
2018-11-22 13:07:06 -0600

    Update single curve reading function

--- src/rw3dm.cpp
+++ src/rw3dm.cpp
@@ -85,7 +85,56 @@ bool writeONFile(py::list &data, std::string file_name)
 
 void _readCurve(const ON_Geometry* geometry, py::dict &data)
 {
-    py::print("Succesfully read curve object!");
+    // We know that "geometry" is a curve object
+    ON_Curve *geoCurve = (ON_Curve *)geometry;
+
+    // Try to get the NURBS form of the curve object
+    ON_NurbsCurve nurbsCurve;
+    if (geoCurve->NurbsCurve(&nurbsCurve))
+    {
+        // Get degree
+        data["degree"] = nurbsCurve.Degree();
+
+        // Get knot vector
+        py::list knotVector;
+        for (int idx = 0; idx < nurbsCurve.KnotCount(); idx++)
+        {
+            knotVector.append(nurbsCurve.Knot(idx));
+        }
+        data["knotvector"] = knotVector;
+
+        py::dict controlPoints;
+
+        // Get control points
+        int num_coords = nurbsCurve.IsRational() ? nurbsCurve.CVSize() - 1 : nurbsCurve.CVSize();
+        py::list points;
+        for (int idx = 0; idx < nurbsCurve.CVCount(); idx++)
+        {
+            double *vertex = nurbsCurve.CV(idx);
+            py::list point;
+            for (int c = 0; c < num_coords; c++)
+            {
+                point.append(vertex[c]);
+            }
+            points.append(point);
+        }
+        controlPoints["points"] = points;
+
+        // Check if the NURBS curve is rational, i.e. weights exist as the last row of control points
+        if (nurbsCurve.IsRational())
+        {
+            // Get weights
+            py::list weights;
+            for (int idx = 0; idx < nurbsCurve.CVCount(); idx++)
+            {
+                double *vertex = nurbsCurve.CV(idx);
+                weights.append(vertex[num_coords - 1]);
+            }
+            controlPoints["weights"] = weights;
+        }
+        data["control_points"] = controlPoints;
+    }
+    //py::print("Succesfully read curve object!");
 }
 
 void _readSurface(const ON_Geometry* geometry, py::dict &data)
