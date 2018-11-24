906a71215c45842c15a9c21a015fdb8913b303d0
Onur Rauf Bingol <orbingol@gmail.com>
2018-11-23 22:45:22 -0600

    Define python function arguments

--- src/bindings.cpp
+++ src/bindings.cpp
@@ -8,6 +8,8 @@ PYBIND11_MODULE(rw3dm, m)
     m.doc() = "Reads and writes OpenNURBS/Rhino .3DM files"; // module docstring
     m.def("start", &startON, "Starts OpenNURBS library");
     m.def("stop", &stopON, "Stops OpenNURBS library");
-    m.def("read", &readONFile, "Reads OpenNURBS/Rhino .3DM files");
-    m.def("write", &writeONFile, "Writes OpenNURBS/Rhino .3DM files");
+    m.def("read", &readONFile, "Reads OpenNURBS/Rhino .3DM files",
+        py::arg("filename"), py::arg("data_out"));
+    m.def("write", &writeONFile, "Writes OpenNURBS/Rhino .3DM files",
+        py::arg("data_in"), py::arg("filename"));
 }
